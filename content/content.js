c0fde91a920be4ad7e8906f5b5e534b0fa3b8bc7
myname21-meet <myname21@meet.mit.edu>
2019-08-01 15:30:00 -0400

    JQuery lab 1 1.1-.1.3

--- content/content.js
+++ content/content.js
@@ -1,1 +1,11 @@
 // Write all your JavaScript and JQuery code in this file! :)
+var count = 0
+$("#counter").on("click", function(){
+	$("h1").eq(1).html(count + 1);
+	count += 1;
+});
+$('#reset').on("click", function(){
+	$("h1").eq(1).html(0);
+	count = 0;
+});
+
--- style/style.js
+++ style/style.js
@@ -3,16 +3,19 @@
 
 
 function updateSize(newSize) {
+	$("#logo").css("width", newSize)
   // FINISH ME! Set the width of the MEET logo to be newSize in pixels.
 }
 
 
 function updateLeftPosition(newX) {
+	$("#logo").css("transform", "rotateX("+ newX + "deg)")
 	// FINISH ME
 
 }
 
 function updateTopPosition(newY) {
+	$("#logo").css('transform', "rotateY("+ newY + "deg)")
 	// FINISH ME
 }
 
